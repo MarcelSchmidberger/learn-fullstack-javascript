0cb9328dc8bf9e995bde37d31a5a04bba5633bec
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-15 18:12:38 +0200

    Add ejs.

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ import fs from 'fs';
 import express from 'express';
 const server = express();
 
+server.set('view engine', 'ejs')
+
 server.listen(config.port,() =>{
     console.info('express listening on port ', config.port)
 });
 
 server.get('/',(req, res)=>{
-    res.send('Hello Express')
+    res.render('index',{
+        content: "Hallo <em>test2</em>"
+    });
 });
 
 server.use(express.static('public'));
