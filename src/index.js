0f0c1302f04830432e0c15f0b2f2fbb87bdf4703
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-15 19:56:36 +0200

    Add JSX syntax.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const color = Math.random() > 0.5 ? 'green' :'red';
+
 ReactDOM.render(
-    React.createElement('h2', null, 'Hello Reacst'),
+    <h2 className="text-center" style={{color}}>Hello React with JSX...test{}</h2>,
     document.getElementById('root')
 );
 
