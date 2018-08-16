4176314eeccb543533d3ed48a87b9a1992ab6a87
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-16 10:21:57 +0200

    Add state to app component.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Header from './Header';
 
 
-const App = () =>{
-    return(
-        <div className= "App">
-            <Header message="naming Contest"/>
-            <div></div>
-        </div>
-    );
-};
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageHeader: 'Naming Contest'
+        };
+    }
+
+    render(){
+        return(
+            <div className= "App">
+                <Header message = {this.state.pageHeader}/>
+                <div></div>
+            </div>
+        );
+    }
+}
 
 
 
