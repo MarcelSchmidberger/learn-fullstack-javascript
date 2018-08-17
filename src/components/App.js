617967925f7fad0c6511d7e11db41e1baaf3ae29
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-17 11:24:59 +0200

    Add Contest detail page on click.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import ContestList from './ContestList';
 import PropTypes from 'prop-types';
+import Contest from './Contest';
 
 const pushState = (obj, url) =>
     window.history.pushState(obj, '', url); // see here https://developer.mozilla.org/de/docs/Web/Guide/DOM/Manipulating_the_browser_history
@@ -21,14 +22,25 @@ class App extends React.Component{
             { currentContestId: contestId },
             `/contest/${contestId}`
         );
+        // lookup the contest
+        this.setState({
+            pageHeader: this.state.contests[contestId].contestName,
+            currentContestId: contestId
+        });
+    }
+    currentContent(){
+        if(this.state.currentContestId){
+            return <Contest {...this.state.contests[this.state.currentContestId]}/>;
+        }
+        return <ContestList 
+            onContestClick= {this.fetchContest}
+            contests = {this.state.contests}/>;
     }
     render(){
         return(
             <div className= "App">
                 <Header message = {this.state.pageHeader}/>
-                <ContestList 
-                    onContestClick= {this.fetchContest}
-                    contests = {this.state.contests}/>
+                {this.currentContent()}
             </div>
         );
     }
@@ -39,7 +51,7 @@ App.defaultProps = {
 };
 
 App.propTypes = {
-    initialContests: PropTypes.array,
+    initialContests: PropTypes.object,
     onContestClick: PropTypes.func.isRequired,
 };
 export default App;
