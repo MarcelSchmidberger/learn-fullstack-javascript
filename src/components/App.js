095991cfe568b18f318cb07ab8536c8dbd68f168
MarcelSchmidberger <marcel.schmidberger@student.hpi.de>
2018-08-17 10:20:19 +0200

    Add history and url navigation to contestlist.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Header from './Header';
-import ContestPreview from './ContestPreview';
+import ContestList from './ContestList';
+import PropTypes from 'prop-types';
+
+const pushState = (obj, url) =>
+    window.history.pushState(obj, '', url); // see here https://developer.mozilla.org/de/docs/Web/Guide/DOM/Manipulating_the_browser_history
+    //this adds links to history, so you can use back button eventhough no page reload has happend
 
 class App extends React.Component{
     state = {
@@ -10,24 +15,31 @@ class App extends React.Component{
     componentDidMount(){
         
     }
+
+    fetchContest = (contestId) => {
+        pushState(
+            { currentContestId: contestId },
+            `/contest/${contestId}`
+        );
+    }
     render(){
         return(
             <div className= "App">
                 <Header message = {this.state.pageHeader}/>
-                <div>
-                    {this.state.contests.map( contest =>
-                        <ContestPreview key ={contest.id} {...contest} />
-                    )}
-                </div>
+                <ContestList 
+                    onContestClick= {this.fetchContest}
+                    contests = {this.state.contests}/>
             </div>
         );
     }
 }
 
-
-
 App.defaultProps = {
     headerMessage: 'Hello!'
 };
 
+App.propTypes = {
+    initialContests: PropTypes.array,
+    onContestClick: PropTypes.func.isRequired,
+};
 export default App;
